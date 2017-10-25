60a00f00ba46a602768b830f36ecc04fb5ff4c4f
李梦龙 <649076408@qq.com>
2017-10-25 21:36:53 +0800

    fix: remove time

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ var VueCookies = {
     if (!key || !this.isKey(key)) {
       return false;
     }
-    document.cookie = encodeURIComponent(key) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT" + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "");
+    document.cookie = encodeURIComponent(key) + "=''; expires="+ new Date() +"" + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "; path=/");
     return true;
   },
   isKey: function(key) {
